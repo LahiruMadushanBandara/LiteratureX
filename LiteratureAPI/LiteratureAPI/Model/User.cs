0eda5cc99e2b95063147d18d737a48b4a7dd15bf
lahiru <lahiru@theavo.net>
2023-03-31 22:52:14 +0530

    create app structure

---
+++ LiteratureAPI/LiteratureAPI/Model/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LiteratureAPI.Model
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
+    }
+}
